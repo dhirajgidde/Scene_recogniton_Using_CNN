ed287c1ec9fd482e0298ec546e6d245a83b392f3
dhiraj_gidde <49560768+dhirajgidde@users.noreply.github.com>
2020-09-25 21:46:49 +0530

    Add files via upload

---
+++ 30K_cnn.py
@@ -0,0 +1,401 @@
+
+
+import os,cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+#from keras.optimizers import SGD,RMSprop,adam
+from keras.layers import Conv2D,GlobalAveragePooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import SGD, RMSprop, Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
+
+
+
+#%%
+
+#PATH = os.getcwd()
+# Define data path
+data_path = 'E:\\75K_Train\\data'
+data_dir_list = os.listdir(data_path)
+
+num_channel=1
+num_epoch=150
+lr_rate=1.0e-4
+
+# Define the number of classes
+num_classes =6
+
+img_data_list=[]
+
+for dataset in data_dir_list:
+	img_list=os.listdir(data_path+'/'+ dataset)
+	print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+	for img in img_list:
+		input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
+		input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+		input_img_resize=cv2.resize(input_img,(128,128))
+		img_data_list.append(input_img_resize)
+
+img_data = np.array(img_data_list)
+img_data = img_data.astype('float32')
+img_data /= 255
+print (img_data.shape)
+
+if num_channel==1:
+	if K.image_dim_ordering()=='th':
+		img_data= np.expand_dims(img_data, axis=1) 
+		print (img_data.shape)
+	else:
+		img_data= np.expand_dims(img_data, axis=4) 
+		print (img_data.shape)
+		
+else:
+	if K.image_dim_ordering()=='th':
+		img_data=np.rollaxis(img_data,3,1)
+		print (img_data.shape)
+		
+
+
+#%%
+# Assigning Labels
+
+# Define the number of classes
+num_classes = 6
+
+num_of_samples = img_data.shape[0]
+labels = np.ones((num_of_samples,),dtype='int64')
+
+labels[0:5000]      = 0
+bels[5000:10000]  = 1
+labels[10000:15000] = 2
+labels[15000:20000] = 3
+labels[20000:25000] = 4
+labels[25000:] = 5
+	  
+names = ['auditorium','bathroom','bedroom','farm','forest','swimming pool']
+#names = ['auditorium','bathroom','bedroom','farm_cultivated','forest','swimmingpool_outdoor']
+
+Y = np_utils.to_categorical(labels, num_classes)
+
+
+x,y = shuffle(img_data,Y, random_state=2)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+
+datagen = ImageDataGenerator(
+    featurewise_center=False,  # set input mean to 0 over the dataset
+    samplewise_center=False,  # set each sample mean to 0
+    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=False,  # apply ZCA whitening
+    rotation_range=45,  # randomly rotate images in the range (degrees, 0 to 180)
+    width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+    height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+    horizontal_flip=True,  # randomly flip images
+    vertical_flip=False)  # randomly flip images
+
+datagen.fit(X_train)
+
+#%%
+# Defining the model
+input_shape=img_data[0].shape
+					
+model = Sequential()
+
+model.add(Conv2D(96, (3, 3), activation='relu', padding="same", input_shape=input_shape))
+
+model.add(Conv2D(96, (3, 3),activation='relu', padding="same", strides=2))
+
+model.add(Dropout(0.5))
+
+
+model.add(Conv2D(192, (3, 3),activation='relu', padding="same"))
+model.add(Conv2D(192, (3, 3),activation='relu', padding="same"))
+model.add(Conv2D(192, (3, 3),activation='relu', padding="same", strides=2))
+model.add(Dropout(0.5))
+
+
+model.add(Conv2D(192, (3, 3), padding="same"))
+model.add(Activation('relu'))
+model.add(Conv2D(192, (1, 1), padding="valid"))
+model.add(Activation('relu'))
+
+model.add(Conv2D(15, (1, 1), padding="valid"))
+model.add(GlobalAveragePooling2D())
+
+model.add(Activation('softmax'))
+
+
+augmented_checkpoint = ModelCheckpoint('augmented_best_model.h5',  # model filename
+                             monitor='val_loss', # quantity to monitor
+                             verbose=0, # verbosity - 0 or 1
+                             save_best_only= True, # The latest best model will not be overwritten
+                             mode='auto')
+
+adam=Adam(lr=lr_rate)
+model.compile(loss='categorical_crossentropy',optimizer=adam,metrics=["accuracy"])
+
+
+model.summary()
+model.get_config()
+model.layers[0].get_config()
+model.layers[0].input_shape			
+model.layers[0].output_shape			
+model.layers[0].get_weights()
+np.shape(model.layers[0].get_weights()[0])
+model.layers[0].trainable
+
+#%%
+# Training
+hist = model.fit(X_train, y_train, nb_epoch=num_epoch, verbose=1, validation_data=(X_test, y_test))
+#hist = model.fit(X_train, y_train, batch_size=32, nb_epoch=20,verbose=1, validation_split=0.2)
+
+# Training with callbacks -
+from keras import callbacks
+
+filename='model_train_new.csv'
+csv_log=callbacks.CSVLogger(filename, separator=',', append=False)
+
+
+ 
+early_stopping=callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='min')
+
+filepath="Best-weights-my_model-{epoch:03d}-{loss:.4f}-{acc:.4f}.hdf5"
+
+checkpoint = callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+callbacks_list = [csv_log,early_stopping,checkpoint]
+
+
+hist = model.fit(X_train, y_train, batch_size=64, nb_epoch=num_epoch, verbose=1, validation_data=(X_test, y_test),callbacks=callbacks_list)
+
+
+#visualizing losses and accuracy
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+#%%
+xc=range(2)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.savefig('train_loss vs val_loss'+'.jpg')
+plt.grid(True)
+plt.legend(['train','val'])
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.savefig('train_acc vs val_acc'+'.jpg')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+#fig.savefig('Graph'+'.jpg')
+
+
+#%%
+
+# Evaluating the model
+
+score = model.evaluate(X_test, y_test,  verbose=0)
+print('Test Loss:', score[0])
+print('Test accuracy:', score[1])
+
+test_image = X_test[0:1]
+print (test_image.shape)
+
+print(model.predict(test_image))
+print(model.predict_classes(test_image))
+print(y_test[0:1])
+
+# Testing a new image
+test_image = cv2.imread('F:\\Test_CNN_ACC\\audi.jpg')
+test_image=cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+test_image=cv2.resize(test_image,(128,128))
+test_image = np.array(test_image)
+test_image = test_image.astype('float32')
+test_image /= 255
+print (test_image.shape)
+   
+if num_channel==1:
+	if K.image_dim_ordering()=='th':
+		test_image= np.expand_dims(test_image, axis=0)
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=3) 
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+		
+else:
+	if K.image_dim_ordering()=='th':
+		test_image=np.rollaxis(test_image,2,0)
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+		
+# Predicting the test image
+print((model.predict(test_image)))
+print("classes:",model.predict_classes(test_image))
+
+#%%
+
+# Visualizing the intermediate layer
+
+#
+def get_featuremaps(model, layer_idx, X_batch):
+	get_activations = K.function([model.layers[0].input, K.learning_phase()],[model.layers[layer_idx].output,])
+	activations = get_activations([X_batch,0])
+	return activations
+
+layer_num=3
+filter_num=0
+
+activations = get_featuremaps(model, int(layer_num),test_image)
+
+print (np.shape(activations))
+feature_maps = activations[0][0]      
+print (np.shape(feature_maps))
+
+if K.image_dim_ordering()=='th':
+	feature_maps=np.rollaxis((np.rollaxis(feature_maps,2,0)),2,0)
+print (feature_maps.shape)
+
+fig=plt.figure(figsize=(16,16))
+plt.imshow(feature_maps[:,:,filter_num],cmap='gray')
+plt.savefig("featuremaps-layer-{}".format(layer_num) + "-filternum-{}".format(filter_num)+'.jpg')
+
+num_of_featuremaps=feature_maps.shape[2]
+fig=plt.figure(figsize=(16,16))	
+plt.title("featuremaps-layer-{}".format(layer_num))
+subplot_num=int(np.ceil(np.sqrt(num_of_featuremaps)))
+for i in range(int(num_of_featuremaps)):
+	ax = fig.add_subplot(subplot_num, subplot_num, i+1)
+	#ax.imshow(output_image[0,:,:,i],interpolation='nearest' ) #to see the first filter
+	ax.imshow(feature_maps[:,:,i],cmap='gray')
+	plt.xticks([])
+	plt.yticks([])
+	plt.tight_layout()
+plt.show()
+fig.savefig("featuremaps-layer-{}".format(layer_num) + '.jpg')
+
+#%%
+# Printing the confusion matrix
+from sklearn.metrics import classification_report,confusion_matrix
+import itertools
+
+Y_pred = model.predict(X_test)
+print(Y_pred)
+y_pred = np.argmax(Y_pred, axis=1)
+print(y_pred)
+#y_pred = model.predict_classes(X_test)
+#print(y_pred)
+#target_names = ['class0(auditorium)','class1(bathroom)','class2(bedroom)','class3(cl)','class4(staircase)','class5(jewelry_shop)','class6(Kitchen)','class7(farm)','class8(forest)','class9(dam)','class10(bridge)','class11(swimming_pool)','class12(railway_track)','class13(waterfall)','class14(temple)']
+target_names = ['class0(auditorium)','class1(bathroom)','class2(bedroom)','class3(farm_cultivated)','class4(forest)','class5(swimmingpool)']	  
+					
+print(classification_report(np.argmax(y_test,axis=1), y_pred,target_names=target_names))
+
+print(confusion_matrix(np.argmax(y_test,axis=1), y_pred))
+
+
+# Plotting the confusion matrix
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('Confusion Matrix'+'.jpg')
+# Compute confusion matrix
+cnf_matrix = (confusion_matrix(np.argmax(y_test,axis=1), y_pred))
+
+np.set_printoptions(precision=2)
+plt.figure()
+
+
+
+# Plot non-normalized confusion matrix
+plot_confusion_matrix(cnf_matrix, classes=target_names,
+                      title='Confusion matrix')
+#plt.figure()
+# Plot normalized confusion matrix
+#plot_confusion_matrix(cnf_matrix, classes=target_names, normalize=True,
+#                      title='Normalized confusion matrix')
+#plt.figure()
+plt.show()
+
+#%%
+# Saving and loading model and weights
+from keras.models import model_from_json
+from keras.models import load_model
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF51111111111111111111
+model.save_weights("model.h5")
+print("Saved model to disk")
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+model.save('model.hdf5')
+loaded_model=load_model('model.hdf5')
+
